b9599d03010df14f5d73db5e4ecf728e5633bf90
Markus <Markus@192.168.0.14>
2016-09-18 16:45:32 +0300

    Add the main program.

---
+++ Myyntitykki2016/src/main/java/com/lampola/myyntitykki/Myyntitykki.java
@@ -0,0 +1,12 @@
+package com.lampola.myyntitykki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myyntitykki {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Myyntitykki.class, args);
+    }
+}
